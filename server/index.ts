1e4493f59fafd6bc3071451976baf6b5cd0e6e29
Umer Ali <umerprocoder@gmail.com>
2026-05-20 12:55:16 +0500

    fix: Change X-Frame-Options to SAMEORIGIN to allow same-origin ad iframe embedding

--- server/index.ts
+++ server/index.ts
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use((_req, res, next) => {
   // Prevent XSS, Clickjacking, and ensure MIME types are respected
   res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
   
